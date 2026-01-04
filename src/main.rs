d0a8c1acfe30c0a2f1dfbd2922f6015976aa2705
Niels Bongers <nielsbongers5@gmail.com>
2026-01-04 18:20:05 +0100

    Working on CLT optimizations but dendrites currently weird

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use dendrite_model::simulation::{SeedType, Simulation};
+use env_logger::{Builder, Env};
+#[allow(unused_imports)]
+use log::{debug, info, warn};
 
 fn main() {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting the simulation!");
+
     let x_max = 1000;
     let y_max = 1000;
     let max_iterations = 100_000;
--- src/simulation/simulation.rs
+++ src/simulation/simulation.rs
@@ -6,9 +6,13 @@ use indicatif::{ProgressBar, ProgressStyle};
 use rand::Rng;
 use rand::SeedableRng;
 use rand::rngs::SmallRng;
+use rand_distr::{Distribution, Normal};
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io::Write;
+
+#[allow(unused_imports)]
+use log::{debug, info, warn};
 
 impl Simulation {
     pub fn new(x_max: i32, y_max: i32, max_iterations: i32, seed_type: &SeedType) -> Self {
@@ -161,6 +165,7 @@ impl Simulation {
         }
     }
 
+    #[cfg(feature = "regular_implementation")]
     pub fn run(&mut self) {
         let progress_bar = ProgressBar::new(self.max_iterations as u64);
         progress_bar.set_style(
@@ -193,50 +198,44 @@ impl Simulation {
             let mut last_check_x = x_init;
             let mut last_check_y = y_init;
 
-            let mut distance_to_closest_point = 0;
+            let mut minimum_distance_to_seed = 0;
 
             let mut brownian_motion_iteration_counter = 0;
             let mut hashmap_iteration_counter = 0;
 
             loop {
                 if enable_hashmap_optimization {
-                    // We check the Manhattan distance to the nearest point - call that $N$ steps.
-                    // Brownian motion takes $\sqrt(N)$ iterations to make that distance, on average, so that can take a _long_ time.
-                    // We recalculate after it has moved $N$ steps from the last check.
-                    // However, if the grid is almost entirely filled, this would be very expensive! We only need to move a few cells to hit something then, while we need to iterate over all $M$ keys in the hashmap, so $\mathcal O(M) \gg \mathcal O(N)$ there.
-                    // So - we keep a moving average of the hashmap access counts and switch between these behaviors dynamically.
-
                     let distance_to_last_check =
                         (last_check_x - x).abs() + (last_check_y - y).abs();
+
                     brownian_motion_iteration_counter += NEIGHBORHOOD_SIZE;
 
-                    if distance_to_last_check >= distance_to_closest_point - 2 {
-                        let (closest_x, closest_y) = self
-                            .lattice
-                            .keys()
-                            .min_by_key(|(particle_x, particle_y)| {
-                                (x - particle_x).abs() + (y - particle_y).abs()
-                            })
-                            .expect("No minimum distance found");
-
+                    // Only if we've travelled near to the radius do we check adjacency!
+                    if distance_to_last_check >= minimum_distance_to_seed - 2 {
                         hashmap_iteration_counter +=
                             self.lattice.keys().len() as i32 + NEIGHBORHOOD_SIZE;
 
-                        distance_to_closest_point = (x - *closest_x).abs() + (y - *closest_y).abs();
+                        minimum_distance_to_seed = self
+                            .lattice
+                            .keys()
+                            .map(|(particle_x, particle_y)| {
+                                (x - particle_x).abs() + (y - particle_y).abs()
+                            })
+                            .min()
+                            .expect("No minimum distance found");
 
                         last_check_x = x;
                         last_check_y = y;
 
                         if self.check_occupied_neighbors(x, y) {
                             self.set_cell(x, y, iteration);
-
                             break;
                         }
                     }
                 } else {
+                    // Otherwise: always check adjacency
                     if self.check_occupied_neighbors(x, y) {
                         self.set_cell(x, y, iteration);
-
                         break;
                     }
                 }
@@ -288,6 +287,142 @@ impl Simulation {
             progress_bar.inc(1);
         }
 
+        progress_bar.finish();
+    }
+
+    #[cfg(feature = "clt")]
+    pub fn run(&mut self) {
+        let progress_bar = ProgressBar::new(self.max_iterations as u64);
+        progress_bar.set_style(
+            ProgressStyle::with_template(
+                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] \
+         {pos:>7}/{len:7} ({eta_precise}) | {msg}",
+            )
+            .unwrap()
+            .progress_chars("=>-"),
+        );
+
+        const DISTRIBUTION_MEAN: f64 = 0.0;
+        const DISTRIBUTION_STD: f64 = 2.0 / 3.0;
+
+        const STEP_COUNT: f64 = 100.0;
+
+        let normal_distribution = Normal::new(DISTRIBUTION_MEAN, STEP_COUNT * DISTRIBUTION_STD)
+            .expect("Failed to initialize normal distribution");
+
+        for iteration in 0..self.max_iterations {
+            let x_init = self.rng.random_range(1..self.x_max);
+            let y_init = self.rng.random_range(1..self.y_max);
+
+            let mut last_check_x = x_init;
+            let mut last_check_y = y_init;
+
+            let mut minimum_distance_to_seed = 0;
+
+            let mut x = x_init;
+            let mut y = y_init;
+
+            if let CellState::Occupied(_) = self.check_cell(x, y) {
+                continue;
+            }
+
+            loop {
+                let distance_to_last_check = (last_check_x - x).abs() + (last_check_y - y).abs();
+
+                let minimum_distance_to_border = *[x, self.x_max - x, y, self.y_max - y]
+                    .iter()
+                    .min()
+                    .expect("Somehow no minimum?!");
+
+                debug!(
+                    "Distance to last check: {}. Minimum distance to seed: {}",
+                    distance_to_last_check, minimum_distance_to_seed
+                );
+
+                let mut check_neighborhood = false;
+
+                // Either initializing or near the edge of the ball!
+                if distance_to_last_check >= minimum_distance_to_seed - 2 {
+                    minimum_distance_to_seed = self
+                        .lattice
+                        .keys()
+                        .map(|(particle_x, particle_y)| {
+                            (x - particle_x).abs() + (y - particle_y).abs()
+                        })
+                        .min()
+                        .expect("No minimum distance found");
+
+                    last_check_x = x;
+                    last_check_y = y;
+
+                    check_neighborhood = true;
+                }
+
+                let maximum_clt_jump_distance =
+                    i32::min(minimum_distance_to_seed, minimum_distance_to_border);
+
+                debug!(
+                    "Minimum distance to seed: {}. Minimum distance to border: {}. Maximum CLT jump distance: {}",
+                    minimum_distance_to_seed, minimum_distance_to_border, maximum_clt_jump_distance
+                );
+
+                if maximum_clt_jump_distance > 50 {
+                    let mut loop_counter = 0;
+
+                    loop {
+                        loop_counter += 1;
+
+                        let x_shift = normal_distribution.sample(&mut self.rng).round() as i32;
+                        let y_shift = normal_distribution.sample(&mut self.rng).round() as i32;
+
+                        let total_distance = x_shift.abs() + y_shift.abs();
+
+                        if total_distance < maximum_clt_jump_distance {
+                            debug!(
+                                "Used CLT! Moved from ({}, {}) to ({}, {}) (shift of {}, {}). Loop counter: {}",
+                                x,
+                                y,
+                                x + x_shift,
+                                y + y_shift,
+                                x_shift,
+                                y_shift,
+                                loop_counter,
+                            );
+
+                            x += x_shift;
+                            y += y_shift;
+
+                            break;
+                        }
+                    }
+                } else {
+                    loop {
+                        let (i, j) = self.random_direction();
+
+                        let x_new = x + i;
+                        let y_new = y + j;
+
+                        if self.check_cell(x_new, y_new) != CellState::Blocked {
+                            x = x_new;
+                            y = y_new;
+
+                            break;
+                        }
+                    }
+                }
+
+                if check_neighborhood {
+                    debug!("Checking neighborhood");
+                    if self.check_occupied_neighbors(x, y) {
+                        self.set_cell(x, y, iteration);
+                        debug!("Found adjacent seed: halting!");
+                        break;
+                    }
+                }
+            }
+
+            progress_bar.inc(1);
+        }
         progress_bar.finish();
     }
 }
